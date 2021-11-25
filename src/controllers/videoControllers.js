11176411001212ce27d8ab0c81d2f29b1aaf3479
saha <jungwonji1960@gmail.com>
2021-11-25 21:53:57 +0900

    템플릿 만들기
    pug,mvp styles 이용
    가짜 데이터베이스를 이용하여 기초 템플릿 작업함(conditionals, iteration,Mixins 이용함)

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,15 +1,36 @@
-export const trending = (req, res) =>res.send("Trending Videos.");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit Video");
-};
-export const search = (req, res) =>res.send("Search");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Video Upload");
 export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    return res.send("Delete Video");
+  return res.send("Delete Video");
 };
--- src/server.js
+++ src/server.js
@@ -6,10 +6,13 @@ import userRouter from './routers/userRouters';
 
 const PORT = 4000;
 
+
 const app = express(); 
 const logger = morgan("dev");
 app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views",process.cwd() + "/src/views");
 app.use("/",globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
